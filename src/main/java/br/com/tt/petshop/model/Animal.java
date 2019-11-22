b4c9291d2f1fd0c6fb88992bb7c840165aeb17dd
Fabiano Rocha <fabianodarocha@gmail.com>
2019-11-22 20:21:33 -0300

    PetShop

--- src/main/java/br/com/tt/petshop/model/Animal.java
+++ src/main/java/br/com/tt/petshop/model/Animal.java
@@ -1,11 +1,27 @@
 package br.com.tt.petshop.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "TB_ANIMAL")
 public class Animal {
 
+    @Id
+    @Column(name = "ID_ANIMAL")
+    private Long id;
+
+    @Column
     private String nome;
+
+    @Column
     private LocalDate dataNascimento;
+
+    //Demarca que não é uma coluna do banco, deve ser ignorada
+    //Mas por favor não usar
+//    @Transient
+//    private boolean flag;
+
 
     public LocalDate getDataNascimento() {
         return dataNascimento;
