a04eae6d544d6a3a4889cb0638ff074d478ae0af
Fabiano Rocha <fabianodarocha@gmail.com>
2019-11-29 19:36:45 -0300

    PetShop

---
+++ src/main/java/br/com/tt/petshop/dto/UnidadeOutDTO.java
@@ -0,0 +1,50 @@
+package br.com.tt.petshop.dto;
+
+public class UnidadeOutDTO {
+
+    private Long id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cnpj;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
